36bb2f16d3dddfea9c0f158e4b757846ef2cf24d
EmaD3c <>
2024-08-20 17:09:30 +0000

    removed last changes by the past one

--- open_shell.c
+++ open_shell.c
@@ -1,60 +1,51 @@
+#include "header.h"
+/**
+ * open_shell - Runs the shell in interactive mode.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of pointers to the arguments.
+ * @envp: The environment variables.
+ *
+ * This function displays a prompt and waits for the user to input commands.
+ * It processes each command by tokenizing the input, checking for built-ins,
+ * and then executing the command if it exists in the PATH.
+ *
+ * Return: Returns 0 on success, or -1 on error.
+ */
 int open_shell(int argc, char *argv[], char *envp[])
 {
-    char *prompt = "$ ";
-    char *line_ptr = NULL;
-    char *tokens[10];
-    int nb_tokens;
-    size_t size_line = 0;
-    ssize_t nbchar_line; /* ssize_t is the correct type for getline return value */
+char *prompt = "$ ";
+char *line_ptr = NULL;
+char *tokens[10];
+int nb_tokens;
+size_t size_line = 0;
+int nbchar_line = 0;
 
-    (void)argc;
-    (void)argv;
-    (void)envp;
+(void)argc;
+(void)argv;
+(void)envp;
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO))
-        {
-            printf("%s", prompt);
-        }
+while (1)
+{
+	if (isatty(STDIN_FILENO))
+		printf("%s", prompt);
+	nbchar_line = getline(&line_ptr, &size_line, stdin);
 
-        nbchar_line = getline(&line_ptr, &size_line, stdin);
+	if (nbchar_line == -1)
+	{
+	free(line_ptr);
+	if (isatty(STDIN_FILENO))
+		printf("\n");
+	return (0);
+	}
 
-        if (nbchar_line == -1)
-        {
-            /* If getline fails, free memory and handle error */
-            if (line_ptr)
-            {
-                free(line_ptr);
-            }
-            if (isatty(STDIN_FILENO))
-            {
-                printf("\n");
-            }
-            return (0); /* or EXIT_FAILURE if appropriate */
-        }
+	if (line_ptr[nbchar_line - 1] == '\n')
+		line_ptr[nbchar_line - 1] = '\0';
 
-        /* Remove newline character from input */
-        if (nbchar_line > 0 &&
-            line_ptr[nbchar_line - 1] == '\n')
-        {
-            line_ptr[nbchar_line - 1] = '\0';
-        }
+	parse(line_ptr, tokens, &nb_tokens);
 
-        parse(line_ptr, tokens, &nb_tokens);
-
-        /* Execute built-in commands or look up and execute commands from PATH */
-        if (execute_builtin(tokens, envp) == -1)
-        {
-            which_path(tokens, nb_tokens, envp, line_ptr);
-        }
-    }
-
-	/*Free line_ptr after exiting loop*/
-    if (line_ptr)
-    {
-        free(line_ptr);
-    }
-	/*or EXIT_SUCCESS if appropriate*/
-    return (0);
+	if (execute_builtin(tokens, envp) == -1)
+		which_path(tokens, nb_tokens, envp, line_ptr);
+}
+free(line_ptr);
+return (0);
 }
